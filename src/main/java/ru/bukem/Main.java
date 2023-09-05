db0a024919f8da1b28c6112331e6ada7d56803c8
Igor Maximov <bif11@mail.ru>
2023-09-05 15:30:05 +0300

    commit error

--- src/main/java/ru/bukem/Main.java
+++ src/main/java/ru/bukem/Main.java
@@ -1,7 +1,8 @@
 package ru.bukem;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Helo !");
+    $Test
+    void mainTest() {
+        System.out.println("Helo World !");
     }
 }
